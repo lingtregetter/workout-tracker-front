7128b6d323e24e0de2ebf9b87fb4c22d27653fdc
lingtregetter <lingtregetter466@gmail.com>
2023-05-20 13:27:02 +0300

    confirmation modal updated

--- www/src/components/confirmationModal/ConfirmationModal.tsx
+++ www/src/components/confirmationModal/ConfirmationModal.tsx
@@ -7,14 +7,19 @@ import { ConfirmationModalProperties } from "../../interfaces/confirmation-modal
 const ConfirmationModal: FC<ConfirmationModalProperties> = (props) => {
   return (
     <Modal onCancel={props.onCancel}>
-      <h2>Do you want to create another workout?</h2>
+      <h2>{props.title}</h2>
+      <h5>{props.children}</h5>
       <div style={{ display: "flex", gap: "30px", justifyContent: "center" }}>
         <Button
           onClick={props.onYesClick}
           text={"Yes"}
           type={"primary"}
         ></Button>
-        <Button onClick={props.onNoClick} text={"No"} type={"primary"}></Button>
+        <Button
+          onClick={props.onNoClick}
+          text={"No"}
+          type={"secondary"}
+        ></Button>
       </div>
     </Modal>
   );
--- www/src/interfaces/confirmation-modal-properties.ts
+++ www/src/interfaces/confirmation-modal-properties.ts
@@ -1,5 +1,9 @@
+import { ReactNode } from "react";
+
 export interface ConfirmationModalProperties {
   readonly onCancel: Function;
   readonly onYesClick: Function;
   readonly onNoClick: Function;
+  readonly title: string;
+  readonly children?: ReactNode;
 }
