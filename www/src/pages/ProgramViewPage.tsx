e25f4edf5951c52fedde519c18f3830b93e6c89b
lingtregetter <lingtregetter466@gmail.com>
2023-05-20 13:28:25 +0300

    Program view page - delete training program

--- www/src/pages/ProgramViewPage.tsx
+++ www/src/pages/ProgramViewPage.tsx
@@ -6,12 +6,16 @@ import Loading from "../components/loading/Loading";
 import OverviewRow from "../components/overview-row/OverviewRow";
 import { useNavigate, useParams } from "react-router-dom";
 import CreateWorkoutContext from "../stores/create-workout-context";
+import Button from "../components/button/Button";
+import ConfirmationModal from "../components/confirmationModal/ConfirmationModal";
 
 const ProgramViewPage: FC = () => {
   const [trainingProgram, setTrainingProgram] = useState<TrainingProgram>();
   const { programId } = useParams();
   const navigate = useNavigate();
   const context = useContext(CreateWorkoutContext);
+  const [isConfirmationModalVisible, setIsConfirmationModalVisible] =
+    useState(false);
 
   useEffect(() => {
     loadProgramBlocks();
@@ -39,6 +43,20 @@ const ProgramViewPage: FC = () => {
     navigate(`/workout/create`);
   };
 
+  const onYesClick = async () => {
+    try {
+      await httpClient().delete(`/v1/TrainingPrograms/${programId}`);
+
+      navigate("/programs");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onNoClick = () => {
+    setIsConfirmationModalVisible(false);
+  };
+
   return (
     <>
       <MainView title={trainingProgram?.programName ?? ""}>
@@ -52,6 +70,25 @@ const ProgramViewPage: FC = () => {
                 onAddClick={() => onAddClick(item.id, item.blockName)}
               ></OverviewRow>
             ))}
+            <Button
+              text={"Delete program"}
+              onClick={() => setIsConfirmationModalVisible(true)}
+              type={"outlined"}
+              style={{ marginTop: "40px" }}
+            ></Button>
+            {isConfirmationModalVisible && (
+              <ConfirmationModal
+                onCancel={() =>
+                  setIsConfirmationModalVisible((isVisible) => !isVisible)
+                }
+                onYesClick={onYesClick}
+                onNoClick={onNoClick}
+                title={`Are you sure you want to delete training program - ${trainingProgram.programName}?`}
+                children={
+                  "You cannot undo this move and all your data about this program will be lost!"
+                }
+              ></ConfirmationModal>
+            )}
           </>
         ) : (
           <Loading />
